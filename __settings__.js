af9b6bc67c59709fd378e85bd385fe894da95b3c
Rajasekhar <rajasekhartechpy@gmail.com>
2021-09-17 05:52:29 +0530

    test

--- __settings__.js
+++ __settings__.js
@@ -8,7 +8,7 @@ CONTEXT_OPTIONS = {
     'preferWebGl2': true,
     'powerPreference': "default"
 };
-SCRIPTS = [ 54335008, 54346967, 54346979, 54347988, 54348003, 54354421, 54354422, 54354423, 54354424, 54354831, 54372734, 54423556, 54439282, 54439283, 54439289, 54743754 ];
+SCRIPTS = [ 54335008, 54346967, 54346979, 54347988, 54348003, 54354421, 54354422, 54354423, 54354424, 54354831, 54372734, 54423556, 54439282, 54439283, 54439289, 54743754, 55546364, 55546516 ];
 CONFIG_FILENAME = "config.json";
 INPUT_SETTINGS = {
     useKeyboard: true,
@@ -18,5 +18,6 @@ INPUT_SETTINGS = {
 };
 pc.script.legacy = false;
 PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/55139875/1/basis.wasm.js', 'wasmUrl' : 'files/assets/55139876/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/55139874/1/basis.js', 'preload' : false},
     {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/54336688/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/54336689/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/54336687/1/ammo.js', 'preload' : true},
 ];
